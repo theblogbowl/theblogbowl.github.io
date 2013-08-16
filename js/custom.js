0bdaef38fdf66e366a3533aafe7edf92d0d32d69
Shaumik <sdaityari@gmail.com>
2013-08-16 19:36:10 +0530

    cleaned up code to be unix friendly

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 //cowntdown function. Set the date by modifying the date in next line (January 01, 2013 00:00:00):
-		var austDay = new Date("December 1, 2013 00:00:00");
+		var austDay = new Date("January 01, 2014 00:00:00");
 			$('#countdown').countdown({until: austDay, layout: '<div class="item"><p>{dn}</p> <span>-{dl}-</span></div> <div class="item"><p>{hn}</p> <span>-{hl}-</span></div> <div class="item"><p>{mn}</p> <span>-{ml}-</span></div> <div class="item"><p>{sn}</p> <span>-{sl}-</span></div>'});
 			$('#year').text(austDay.getFullYear());
 
-//function for the social hover effect - tooltips		
+//function for the social hover effect - tooltips
 	$('.tooltip').tipsy
 	({
 		fade: true,
@@ -26,7 +26,7 @@ $(document).ready(function() {
 			$("#home").slideDown("slow");
 		}
 	}
-	
+
 //run contact form when the ribbon is clicked
-	$(".contact").click(function(){contact()});	
-});	
+	$(".contact").click(function(){contact()});
+});
